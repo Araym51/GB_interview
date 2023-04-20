2c4787bbd14a830f3bdee28a5aaeb5deeadb8718
Egor Ostroumov <araimo@yandex.ru>
2023-04-20 21:36:35 +0300

    task 4.4

--- interview/main_app/models.py
+++ interview/main_app/models.py
@@ -6,9 +6,12 @@ from django.db import models
 название, дату поступления, цену, единицу измерения, имя поставщика. Выполнить миграции.
 """
 
-class GoodsInformation(models.Model):
+class ProductInformation(models.Model):
     product_name = models.CharField(max_length=256)
     date_of_receipt = models.DateTimeField(auto_now_add=True)
     price = models.DecimalField(max_digits=8, decimal_places=2)
     units_of_measurement = models.CharField(max_length=128, null=True, blank=True)
     provider = models.CharField(max_length=256)
+
+    def __str__(self):
+        return f'Product: {self.product_name}.'
