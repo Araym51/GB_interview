1bab90d0cadf01509305255abf39222f1ee1f388
araym <araimo@yandex.ru>
2023-04-30 17:51:02 +0300

    task 5

--- interview/main_app/views.py
+++ interview/main_app/views.py
@@ -1,5 +1,6 @@
-from django.http import HttpResponseRedirect
+from django.http import JsonResponse
 from django.shortcuts import render
+from django.template.loader import render_to_string
 
 from .forms import ProductInformationForm
 from .models import ProductInformation
@@ -8,16 +9,23 @@ from .models import ProductInformation
 
 
 def products_veiw(request):
-    return render(request, 'main_app/goods_list.html', {'object_list': ProductInformation.objects.all()})
+    return render(request, 'main_app/index.html', {'products': ProductInformation.objects.all()})
 
 
 def create_product(request):
+    data = dict()
     if request.method == 'POST':
         form = ProductInformationForm(request.POST)
         if form.is_valid():
             form.save()
-            return HttpResponseRedirect('/')
-    else:
-        form = ProductInformationForm()
+            data['form_is_valid'] = True
+            products = ProductInformation.objects.all()
+            data['products_html'] = render_to_string('main_app/goods_list.html', {'products': products})
+        else:
+            data['form_html'] = render_to_string('main_app/good_create.html', {'form': form}, request=request)
 
-    return render(request, 'main_app/good_create.html', {'form': form})
+    else:
+        data['form_is_valid'] = False
+        data['form_html'] = render_to_string('main_app/good_create.html', {'form': ProductInformationForm()}, request=request)
+
+    return JsonResponse(data)
